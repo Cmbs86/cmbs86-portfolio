62b7acddf27ff1f9795ad43976c910f0b6910b4e
Carlos Sousa <carlosmanuel.desousa@digitalcareerinstitute.org>
2024-04-22 21:01:44 +0200

    added new text - correct UI

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,20 +1,31 @@
-
-
 function Intro() {
   return (
     <>
       <div id="about" className="introduction">
         <h1 className="introduction_header">
-          Hey, I'm Carlos Sousa, a 37-year-old DCI student diving headfirst into
-          the world of web development.
+          Hi, I'm Carlos Sousa.
+          <br />A 38-year-old Junior Full Stack Web Developer.
         </h1>
         <p className="introduction_text">
-          I may not have all the answers, but I've got determination and a knack
-          for finding bugs (the digital kind, not the creepy-crawly ones). Join
-          me on this web development journey, where we embrace the quirks,
-          celebrate the small wins, and have a good laugh at the occasional
-          digital mishap. "Let's make the web a bit more interesting, one line
-          of code at a time."
+          Transitioning from an 8-year career as a cook, I've found my passion
+          in crafting digital experiences as a web developer. My expertise lies
+          in the MERN stack: using React.js for dynamic frontends, Node.js and
+          Express.js for robust backends, and MongoDB for flexible data storage.
+          <br />
+          <br />
+          When it comes to creating interactive web functionality, I turn to
+          JavaScript, weaving together engaging user experiences. From the
+          layout to the visual appeal, I meticulously shape the structure with
+          HTML and breathe life into designs with CSS. Drawing from my culinary
+          background, I bring creativity, attention to detail, and a love for
+          problem-solving into every line of code. Continuously studying and
+          learning new technologies is a core part of my journey.
+          <br />
+          <br />
+          Outside the world of coding, you'll often find me lost in the rhythms
+          of my bass guitar. I've played in 2 band projects in the past and
+          currently working on another musical venture. Let's collaborate and
+          turn your ideas into beautifully functional web solutions.
         </p>
       </div>
     </>
