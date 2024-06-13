fa7bed5955702d06626773ec24d8cbace9030c9f
Carlos Sousa <carlosmanuel.desousa@digitalcareerinstitute.org>
2024-06-13 13:48:53 +0200

    refactor: Update Footer component with ContactCard component and copyright information

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,3 @@
-
-
 //react imports
 import { IconContext } from "react-icons";
 import { IoLogoJavascript } from "react-icons/io5";
@@ -19,12 +17,11 @@ import akpica from "../assets/images/akpica-ecommerce.png";
 import theprojects from "../assets/images/the-projects.jpeg";
 import firstportfolio from "../assets/images/first-portfolio.png";
 
-
 function Main() {
   return (
     <>
-        <main className="skillset_section">
-      <IconContext.Provider value={{ className: "skill-card-icons"}}>
+      <main className="skillset_section">
+        <IconContext.Provider value={{ className: "skill-card-icons" }}>
           <section className="skills">
             <div className="card card_1">
               <h3 className="card_text-1">Languages:</h3>
@@ -47,102 +44,63 @@ function Main() {
           </section>
         </IconContext.Provider>
 
-          <h3 className="project_text">
-            My Latest Projects:
-          </h3>
+        <h3 className="project_text">My Latest Projects:</h3>
 
-          <section id="projects-section" className="projects-section">
-            <div className="grid_card projects_card1">
-              <ProjectCard
-            image={akpica}
-            altTitle={"akpica-ecommerce"}
-            imageUrl={"https://akpica.netlify.app/"}
-            title={"akpica ecommerce"}
-            description={
-              "This eCommerce platform is designed to provide users with a range of features that make browsing, selecting, and purchasing products."
-            }
-            demoUrl={"https://akpica.netlify.app/"}
-            codeUrl={"https://github.com/Cmbs86/akpica-ecommerce"}
-          />
-              {/* <span className="top"></span>
-              <span className="right"></span>
-              <span className="bottom"></span>
-              <span className="left"></span> */}
-              
-            </div>
-
-            <div className="grid_card projects_card2">
+        <section id="projects-section" className="projects-section">
+          <div className="grid_card projects_card1">
             <ProjectCard
-            image={theprojects}
-            altTitle={"the-projects"}
-            imageUrl={"https://the-projects-js.netlify.app/#"}
-            title={"The Projects"}
-            description={
-              "JavaScript Projects for mastering the art of web development through hands-on practice with Vanilla JavaScript."
-            }
-            demoUrl={"https://the-projects-js.netlify.app/#"}
-            codeUrl={"https://github.com/Cmbs86/the-projects"}
-          />
-              {/* <span className="top"></span>
+              image={akpica}
+              altTitle={"akpica-ecommerce"}
+              imageUrl={"https://akpica.netlify.app/"}
+              title={"akpica ecommerce"}
+              description={
+                "This eCommerce platform is designed to provide users with a range of features that make browsing, selecting, and purchasing products."
+              }
+              demoUrl={"https://akpica.netlify.app/"}
+              codeUrl={"https://github.com/Cmbs86/akpica-ecommerce"}
+            />
+            {/* <span className="top"></span>
               <span className="right"></span>
               <span className="bottom"></span>
               <span className="left"></span> */}
-            </div>
-
-            <div className="grid_card projects_card3">
-            <ProjectCard
-            image={firstportfolio}
-            altTitle={"first-portfolio"}
-            imageUrl={"https://cmbs86-dci-portfolio.netlify.app/"}
-            title={"first DCI Project"}
-            description={
-              "My first project practicing HTML and CSS, showcasing foundational skills in user interface design, a key step in my web development journey. "
-            }
-            demoUrl={"https://cmbs86-dci-portfolio.netlify.app/"}
-            codeUrl={"https://github.com/Cmbs86/first-portfolio"}
-          />
-              {/* <span className="top"></span>
-              <span className="right"></span>
-              <span className="bottom"></span>
-              <span className="left"></span> */}
-            </div>
-          </section>
-          <div className="form">
-            <h3>Contact me!</h3>
-            {/* <p>Fill in the form below, and I'll get back to you!</p> */}
-            {/* <form id="contact" action="#" method="post">
-              <label for="name">Name:</label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                placeholder="Enter your name here"
-                required
-              />
-
-              <label for="email">Email:</label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                placeholder="Enter your email"
-                required
-              />
-
-              <label for="message">How can I help you?</label>
-              <textarea
-                id="message"
-                name="message"
-                placeholder="Enter your message here"
-                rows="5"
-                required
-              ></textarea>
-
-              <input type="submit" value="Submit" />
-            </form> */}
           </div>
-        </main>
-   
+
+          <div className="grid_card projects_card2">
+            <ProjectCard
+              image={theprojects}
+              altTitle={"the-projects"}
+              imageUrl={"https://the-projects-js.netlify.app/#"}
+              title={"The Projects"}
+              description={
+                "JavaScript Projects for mastering the art of web development through hands-on practice with Vanilla JavaScript."
+              }
+              demoUrl={"https://the-projects-js.netlify.app/#"}
+              codeUrl={"https://github.com/Cmbs86/the-projects"}
+            />
+            {/* <span className="top"></span>
+              <span className="right"></span>
+              <span className="bottom"></span>
+              <span className="left"></span> */}
+          </div>
+
+          <div className="grid_card projects_card3">
+            <ProjectCard
+              image={firstportfolio}
+              altTitle={"first-portfolio"}
+              imageUrl={"https://cmbs86-dci-portfolio.netlify.app/"}
+              title={"first DCI Project"}
+              description={
+                "My first project practicing HTML and CSS, showcasing foundational skills in user interface design, a key step in my web development journey. "
+              }
+              demoUrl={"https://cmbs86-dci-portfolio.netlify.app/"}
+              codeUrl={"https://github.com/Cmbs86/first-portfolio"}
+            />
+          </div>
+        </section>
+        <div className="form">
+          <h3>Contact me:</h3>
+        </div>
+      </main>
     </>
   );
 }
